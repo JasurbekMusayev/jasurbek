32c68eb236c78cb22b60c81a07136afa543e9ec7
JasurbekMusayev <musayevjasurbek12@gmail.com>
2024-02-01 17:18:37 +0300

    gg

---
+++ app/src/main/java/com/example/forex/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.forex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.forex.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.forex2)
+    }
+}
